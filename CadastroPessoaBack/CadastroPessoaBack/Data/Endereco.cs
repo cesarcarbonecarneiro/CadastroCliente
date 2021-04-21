e386f0164b16208fed57a9367c7092f232d96e50
cesar.carneiro <cesar.carneiro2@ufla.br>
2021-04-21 10:54:41 -0300

    front

---
+++ CadastroPessoaBack/CadastroPessoaBack/Data/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoaBack.Data
+{
+    public class Endereco
+    {
+        public virtual int Id { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+        public virtual string Distrito { get; set; }
+        public virtual string Bairro { get; set; }
+        public virtual string Logradouro { get; set; }
+        public virtual string Numero { get; set; }
+        public virtual string Complemento { get; set; }
+    }
+}
